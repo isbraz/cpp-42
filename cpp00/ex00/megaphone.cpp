0b5f127b5b6721d7a0972a2eb0a058010bb8f44d
Ismael Braz <ismael.braz03@hotmail.com>
2024-03-21 19:40:08 +0000

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctype.h>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; av[i]; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+		{
+			if (islower(av[i][j]))
+				std::cout << (char)toupper(av[i][j]);
+			else
+				std::cout << av[1][j];
+		}
+		if (i < ac - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}
