869b6bed5b6425b7ec536f842bb7f816eb372beb
Ismael Braz <ismael.braz03@hotmail.com>
2024-04-16 18:47:15 +0100

    start cpp01/ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cout << "Wrong number of arguments!" << std::endl;
+		return (0);
+	}
+
+}
+	
